79cec8a37b5f3387b501e02bc289c3f2521aa601
zweiqin <zweiqin88@163.com>
2024-04-01 12:13:20 +0800

    2024.04.01 - 更新路径

--- config/test.env.js
+++ config/test.env.js
@@ -3,7 +3,7 @@ module.exports = {
   ENV_CONFIG: '"test"',
   // 团蜂测试
   BASE_API: '"https://test.zhult.com/samrtWorker"',
-  BASE_SHOWAPI: '"https://bus.deyisoft.cn:8890/tfshop"',
-  BASE_COMMUNITY_API: '"https://test.tuanfengkeji.cn/dts-app-api"', // ! 旧商城接口
-  BASE_SHOWAPI: '"https://nsappapitest.tuanfengkeji.cn"'
+  BASE_TIPS_WS_API: '"wss://test.zhult.com/laoa-huozhu/webSocket/shequ20230309"',
+  BASE_SHOWAPI: '"https://nsappapitest.tuanfengkeji.cn"',
+  BASE_COMMUNITY_API: '"https://test.tuanfengkeji.cn/dts-app-api"' // ! 旧商城接口
 }
